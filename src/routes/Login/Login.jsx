42f50d3d9b7cf72efd52329710784f14167ad843
Facu137 <phakuntho@gmail.com>
2024-04-25 10:28:40 -0300

    modificacion de login

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -2,30 +2,75 @@ import './Login.css';
 
 export const Login = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="exampleInputEmail1">
-          Correo
-        </label>
-        <input
-          type="email"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-      </div>
-      <div>
-        <label htmlFor="exampleInputPassword1">
-          Contraseña
-        </label>
-        <input
-          type="password"
-          id="exampleInputPassword1"
-        />
+<div class="wrapper">
+      <span class="icon-close">
+        <ion-icon name="close-outline">
+          X
+        </ion-icon></span>
+
+      <div class="form-box login">
+        <h2>Iniciar Sesion</h2>
+        <form action="#">
+          <div class="input-box">
+            <span class="icon"><ion-icon name="mail"></ion-icon></span>
+            <input type="email" required />
+            <label>Correo</label>
+          </div>
+          <div class="input-box">
+            <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+            <input type="Contraseña" required />
+            <label>Contraseña</label>
+          </div>
+          <div class="remember-forgot">
+            <label
+              ><input type="checkbox" />
+              Recuerdame
+            </label>
+            <a href="#">Olvide mi contraseña</a>
+          </div>
+
+          <button type="submit" class="btn">ingresar</button>
+          <div class="login-register">
+            <p>
+              No tienes una cuenta?
+              <a href="#" class="register-link">Registrate</a>
+            </p>
+          </div>
+        </form>
       </div>
 
-      <button type="submit">
-        Iniciar Sesion
-      </button>
-    </form>
+      <div class="form-box register">
+        <h2>Registrate</h2>
+        <form action="#">
+          <div class="input-box">
+            <span class="icon"><ion-icon name="person"></ion-icon></span>
+            <input type="text" required />
+            <label>Nombre de usuario</label>
+          </div>
+          <div class="input-box">
+            <span class="icon"><ion-icon name="mail"></ion-icon></span>
+            <input type="email" required />
+            <label>Correo</label>
+          </div>
+          <div class="input-box">
+            <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+            <input type="Contraseña" required />
+            <label>Contraseña</label>
+          </div>
+          <div class="remember-forgot">
+            <label
+              ><input type="checkbox" />
+              Acepto los terminos y condiciones
+            </label>
+          </div>
+          <button type="submit" class="btn">Registrarse</button>
+          <div class="login-register">
+            <p>
+              ya tienes una cuenta? <a href="#" class="login-link">ingresa</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
