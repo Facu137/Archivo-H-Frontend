835c46af21b9f756c51cb49ffb6c2480c0347789
FranCODER1 <166779708+FranCODER1@users.noreply.github.com>
2024-07-03 17:35:40 -0300

    Home y Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,33 @@
 import './Footer.css';
+
 export const Footer = () => {
   return (
-<footer>
-  <h3>Footer</h3>
-</footer>  )
-}
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-logo">
+          <img src="/building.jpg" alt="Logo" />
+        </div>
+
+        <div className="footer-links">
+          <ul>
+            <li><a href="#">Acerca de Nosotros</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-social">
+          <ul>
+            <li><a href="#"><i class="fab fa-facebook-f"></i> Facebook</a></li>
+            <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer-copyright">
+        <p>&copy; 2024 Todos los derechos reservados. <a href="#">NOMBRE DE LA EMPRESA</a></p>
+      </div>
+    </footer>
+  );
+};
