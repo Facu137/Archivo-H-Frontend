38eb38ea2ba81bdf75d7ddf64fa1eb2c265c952b
FranCODER1 <166779708+FranCODER1@users.noreply.github.com>
2024-07-03 21:00:01 -0300

    footer modificado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,28 @@
+import React from 'react';
 import './Footer.css';
+import buildingImage from '../../assets/logo.png';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-logo">
-          <img src="/building.jpg" alt="Logo" />
-        </div>
+        <img src={buildingImage} alt="Logo" /> {/* Usa la imagen importada */}  
+         </div>
 
         <div className="footer-links">
-          <ul>
-            <li><a href="#">Acerca de Nosotros</a></li>
-            <li><a href="#">Servicios</a></li>
-            <li><a href="#">Contacto</a></li>
+        <ul>
+            <li>Mitre 127, Santiago del Estero, Argentina</li>
+            <li>(0385) 428 8472</li>
           </ul>
         </div>
 
         <div className="footer-social">
           <ul>
-            <li><a href="#"><i class="fab fa-facebook-f"></i> Facebook</a></li>
-            <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
-            <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
+            <li><a href="https://www.facebook.com/profile.php?id=100070518997083"><i class="fab fa-facebook-f"></i> Facebook</a></li>
           </ul>
         </div>
-      </div>
-
-      <div className="footer-copyright">
-        <p>&copy; 2024 Todos los derechos reservados. <a href="#">NOMBRE DE LA EMPRESA</a></p>
       </div>
     </footer>
   );
