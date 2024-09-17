d4d072f22749b98b2b415bc3158a22d929f62038
Facu137 <phakuntho@gmail.com>
2024-09-17 17:14:40 -0300

    Terminado la lista de posibles empleados

--- src/pages/GestionarEmpleados/PossibleEmployeesList/PossibleEmployeesList.jsx
+++ src/pages/GestionarEmpleados/PossibleEmployeesList/PossibleEmployeesList.jsx
@@ -1,6 +1,7 @@
 // src/pages/GestionarEmpleados/PossibleEmployeesList/PossibleEmployeesList.jsx
 import PropTypes from 'prop-types'
 import UserCard from '../../../components/UserCard/UserCard'
+import possibleEmployeesImage from '../../../assets/topaz-museo_historico_4.avif'
 import './PossibleEmployeesList.css'
 
 const PossibleEmployeesList = ({
@@ -12,25 +13,59 @@ const PossibleEmployeesList = ({
   if (possibleEmployees.length === 0) {
     return <div>No se encontraron posibles empleados.</div>
   }
+
   const handleAccept = async (employeeId) => {
-    await onAccept(employeeId) // Llama a la función onAccept que viene del padre
-    onUpdateCurrentEmployees() // Llama a la función para actualizar la lista de empleados actuales
+    await onAccept(employeeId)
+    onUpdateCurrentEmployees()
   }
 
   return (
-    <div className="employee-list">
-      {possibleEmployees.map((employee) => (
-        <div key={employee.id} className="employee-card-container">
-          <UserCard user={employee} />
-          <div className="buttons-container">
-            <button onClick={() => handleAccept(employee.id)}>Aceptar</button>
-            <button onClick={() => onReject(employee.id)}>Rechazar</button>
+    <div className="possible-employees-section">
+      <div className="possible-employees-card-container">
+        <div className="possible-employees-card-config possible-employee-list-container">
+          <h3>Solicitudes de Nuevos Empleados</h3>
+          <div className="possible-employee-list">
+            {possibleEmployees.map((employee) => (
+              <div
+                key={employee.id}
+                className="possible-employee-listcard-container"
+              >
+                <UserCard user={employee} />
+                <div className="buttons-container">
+                  <button onClick={() => handleAccept(employee.id)}>
+                    Aceptar
+                  </button>
+                  <button onClick={() => onReject(employee.id)}>
+                    Rechazar
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
+          <p className="scroll-hint">
+            Desplázate horizontalmente para ver más.
+          </p>
         </div>
-      ))}
+        <div className="card-config description-card">
+          <h3>Gestionar Solicitudes de Nuevos Empleados</h3>
+          <img
+            src={possibleEmployeesImage}
+            alt="Posibles Empleados"
+            className="possible-employees-image"
+          />
+          <p>
+            Aquí puedes aceptar o rechazar las solicitudes de nuevos empleados.
+          </p>
+          <p>
+            En ambos casos, los usuarios serán notificados por correo
+            electrónico.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
+
 PossibleEmployeesList.propTypes = {
   possibleEmployees: PropTypes.arrayOf(
     PropTypes.shape({
