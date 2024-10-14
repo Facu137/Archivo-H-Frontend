8b2745c8cf3e7e0ed4dc2dd8ac392decf1bd71ba
FrancoRivero <franhd36@gmail.com>
2024-10-14 20:40:00 -0300

    Institucional Slider-Gallery + Componente nuevo InstitucionalCard + Imagenes

--- src/pages/Institucional/Institucional.jsx
+++ src/pages/Institucional/Institucional.jsx
@@ -1,55 +1,102 @@
-// src/pages/Institucional/Institucional.jsx
 import React from 'react'
-import acercaImg from '../../assets/topaz-museo_historico_4.avif'
-import evolucionImg from '../../assets/topaz-hoja-AH.avif'
-import ubicacionImg from '../../assets/topaz-inaguracion-AH.avif'
+import InstitucionalCard from '../../components/InstitucionalCard/InstitucionalCard'
+import libro from '../../assets/libro.avif'
+import libro2 from '../../assets/libro2.avif'
+import img1728590488906 from '../../assets/1728590488906.avif'
+import personas from '../../assets/personas.avif'
+import archivo from '../../assets/archivo.avif'
+import img1728590524186 from '../../assets/1728590524186.avif'
+import img43 from '../../assets/img43.avif'
+import img48 from '../../assets/img48.avif'
+import img49 from '../../assets/img49.avif'
+import img69 from '../../assets/img69.avif'
+import img72 from '../../assets/img72.avif'
+import img77 from '../../assets/img77.avif'
+import img78 from '../../assets/img78.avif'
+import img79 from '../../assets/img79.avif'
+import img80 from '../../assets/img80.avif'
+import img81 from '../../assets/img81.avif'
+import img95 from '../../assets/img95.avif'
+import img105 from '../../assets/img105.avif'
+import img115 from '../../assets/img115.avif'
+import img124 from '../../assets/img124.avif'
+import img129 from '../../assets/img129.avif'
+import img134 from '../../assets/img134.avif'
+import img149 from '../../assets/img149.avif'
+import img154 from '../../assets/img154.avif'
+
 import './Institucional.css'
 
-export const Institucional = () => {
+const Institucional = () => {
   return (
     <div className="institucional-container">
-      <div className="navbar-spacer-institucional"></div>
-      <div className="body-1">
-        <img src={acercaImg} alt="Acerca de" className="body-1-img" />
-        <div className="body-1-content">
-          <h3>Acerca de</h3>
-          <p>
-            El Archivo Histórico de Santiago del Estero se creó para organizar,
-            custodiar y poner a disposición del público documentos históricos.
-            El edificio, ubicado en la calle Mitre 127, tiene un hall de
-            entrada, sala de investigadores, depósito de 140 m2, oficinas y un
-            patio cercano al depósito.
-          </p>
-        </div>
-      </div>
-      <div className="body-2">
-        <img src={evolucionImg} alt="Evolución" className="body-2-img" />
-        <div className="body-2-content">
-          <h3>Evolución</h3>
-          <p>
-            Desde su creación en 2008, se encuentran abocados a la recuperación
-            de toda la documentación histórica. Se confeccionaron los índices,
-            se preservan los documentos con criterios archivísticos y de
-            conservación. Entre los documentos más valiosos se encuentran el
-            Acta de la Autonomía, las primeras Constituciones provinciales
-            manuscritas y la documentación del período colonial, desde 1554, que
-            se sistematizó y editó en varios tomos.
-          </p>
-        </div>
-      </div>
-      <div className="body-3">
-        <img src={ubicacionImg} alt="Ubicación actual" className="body-3-img" />
-        <div className="body-3-content">
-          <h3>Ubicación actual</h3>
-          <p>
-            En el año 2014 el gobierno de la provincia adquirió la propiedad que
-            fuera la casa de Dn Andrés Chazarreta para el funcionamiento de
-            Archivo Histórico, preservando las dos salas donde funcionaba el
-            Museo Chazarreta. Tras las reformas y construcciones pertinentes se
-            inauguró en agosto de 2021.
-          </p>
-        </div>
-      </div>
+      <InstitucionalCard
+        title="Fundación del Archivo General"
+        text="El Archivo General de Santiago del Estero fue fundado el 26 de Agosto de 1910, mediante Ley N° 267 por la Cámara de Representantes de la Provincia."
+        images={[libro, libro2]}
+      />
+
+      <InstitucionalCard
+        title="Organización Sistemática"
+        text="En el año 1941, bajo el gobierno del Dr.José Ignacio Cáceres, se aprobó la Ley N° 1733, razón por la cual se realiza la organización sistemática del Archivo General, creándose entre otras, la sección “documentos históricos”."
+        images={[personas, archivo, img1728590488906]}
+      />
+
+      <InstitucionalCard
+        title="Diciembre de 1993: El Santiagueñazo"
+        text="Con los acontecimientos acaecidos en el Santiagueñazo, el fuego y el agua dejaron graves secuelas en el Archivo General, perdiéndose para siempre numerosos documentos. El Archivo General funcionaba en el Subsuelo del Palacio de los Tribunales"
+        images={[img1728590524186, img43]}
+      />
+      <InstitucionalCard
+        title="El incendio"
+        text="El Archivo General funcionaba en el Subsuelo del Palacio de los Tribunales"
+        images={[img48, img49]}
+      />
+
+      <InstitucionalCard
+        title="El Archivo Histórico"
+        text="Fue creado el 05 de febrero de 2008 mediante decreto N°110/08, 
+        por el actual Gobierno de la Provincia. Esta integrado por todos los documentos anteriores al año 1.900 que formaban 
+        parte de la “sección histórica” del Archivo General. Estas fuentes documentales son: 
+        Expedientes Judiciales, Protocolos Notariales, Tierras Fiscales, Documentos Coloniales, 
+        Correspondencias, Expedientes de Gobierno, Leyes y Decretos, Mensuras, Libros, Boletines y Revistas, Hemeroteca."
+        images={[]} // Sin imágenes
+      />
+
+      <InstitucionalCard
+        title="Se recuperaron"
+        text="Las Actas Capitulares originales, y con ellas el ACTA DE LA AUTONOMIA"
+        images={[img69, img72]}
+      />
+
+      <InstitucionalCard
+        title="Algunas colecciones valiosas"
+        images={[img77, img78]} // Sin texto
+      />
+
+      <InstitucionalCard
+        title="Documentos antiguos"
+        images={[img80, img79, img81]} // Sin texto
+      />
+
+      <InstitucionalCard
+        title="Remodelación"
+        text="El 3 de marzo del año 2008, comenzó la remodelación de su nuevo local"
+        images={[img95, img105, img115]}
+      />
+
+      <InstitucionalCard
+        title="Digitalizaciones"
+        text="Estamos realizando digitalizaciones, llegando hasta la fecha unas 287.400 fotografías."
+        images={[img124, img129, img134, img149, img154]}
+      />
+
+      <InstitucionalCard
+        text="Es necesario que todos asumamos el compromiso de proteger y conservar los fondos documentales, que son patrimonio de los pueblos, y asegurar los mismos para las generaciones futuras. En julio de 2021, inauguramos nuestra nueva cede y definitiva, en Calle Mitre 127, donde fuera la casa del Patriarca del Folklore Argentino, Don Andrés A. Chazarreta, la cual logramos recuperar y donde hoy funcionan las Salas Museos Cahzarreta y el Archivo Histórico de la Provincia." // Sin título
+        images={[]} // Sin imágenes
+      />
     </div>
   )
 }
+
+export default Institucional
