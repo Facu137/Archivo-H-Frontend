5e2068d4eae9ce1301c85b9488e4325a5b4391da
Facu137 <phakuntho@gmail.com>
2024-11-27 15:46:26 -0300

    Instalado bootstrap 5.3.3

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
